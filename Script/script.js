57fdc830f11f60d34ffff1327ff922914b686b39
Salim Sassi <salimsassi139@gmail.com>
2018-04-22 11:18:00 +0100

    up

--- Script/script.js
+++ Script/script.js
@@ -50,8 +50,12 @@ function scrollFunction() {
     }
 };
 
-
-
-
-
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+};
 
